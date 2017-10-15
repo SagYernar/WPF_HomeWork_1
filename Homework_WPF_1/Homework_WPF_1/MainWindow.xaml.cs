52f67feb68634ae04b361a3bf4c7abb5d3106221
Ернар Сагадиев <sagyernar@gmail.com>
2017-10-15 15:10:14 +0600

    AllDone

--- Homework_WPF_1/Homework_WPF_1/MainWindow.xaml.cs
+++ Homework_WPF_1/Homework_WPF_1/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace Homework_WPF_1
 {
@@ -20,19 +22,62 @@ namespace Homework_WPF_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<User> users;
+        private User CurrentUser;
+        private bool nameIsFree;
+        BinaryFormatter formater;
         public MainWindow()
         {
             InitializeComponent();
+            users = new List<User>();
+            CurrentUser = new User();
+            formater = new BinaryFormatter();
+            nameIsFree = true;
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!string.IsNullOrWhiteSpace(userNameSaveTextBox.Text) &&
+                userNameSaveTextBox.Text != "User Name" )
+            {
+                foreach(User user in users)
+                {
+                    if(user.Name == userNameSaveTextBox.Text)
+                    {
+                        nameIsFree = false;
+                    }
+                }
+                if (nameIsFree)
+                {
+                    CurrentUser.Name = userNameSaveTextBox.Text;
+                    CurrentUser.Red = (byte)redSlider.Value;
+                    CurrentUser.Green = (byte)greenSlider.Value;
+                    CurrentUser.Blue = (byte)blueSlider.Value;
+                    users.Add(CurrentUser);
+                                        
+                    using (FileStream stream = new FileStream("users.bin", FileMode.OpenOrCreate))
+                    {
+                        formater.Serialize(stream, users);
+                    }
+                }
+            }
         }
 
         private void LoadButton_Click(object sender, RoutedEventArgs e)
         {
-
+            using (FileStream stream = new FileStream("users.bin", FileMode.OpenOrCreate))
+            {
+                users = (List<User>)formater.Deserialize(stream);
+            }
+            foreach (User user in users)
+            {
+                if (user.Name == userNameLoadTextBox.Text)
+                {
+                    redSlider.Value = user.Red;
+                    greenSlider.Value = user.Green;
+                    blueSlider.Value = user.Blue;
+                }
+            }
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
---
+++ Homework_WPF_1/Homework_WPF_1/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_WPF_1
+{
+    [Serializable]
+    public class User
+    {
+        public string Name { get; set; }
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+
+    }
+}
